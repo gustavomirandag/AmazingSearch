932af7a2970648b603a39ed57658b99cfeadb10d
gustavomirandag <gustavomirandag@gmail.com>
2019-08-10 15:09:49 -0300

    Added ignored file.

---
+++ DataAccess/Services/Elastic/Config/Log/SystemActivityLogElasticConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Services.Elastic.Config.Log
+{
+    public class SystemActivityLogElasticConfig : IElasticConfig
+    {
+        string IElasticConfig.IndexName => DataAccess.Properties.Resources.ResourceManager.GetString("SystemActivityLogIndexName");
+
+        string IElasticConfig.ElastisearchUrl => DataAccess.Properties.Resources.ResourceManager.GetString("ElasticsearchUrl");
+    }
+}
---
+++ DataAccess/Services/Elastic/Config/Log/UserActivityLogElasticConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Services.Elastic.Config.Log
+{
+    public class UserActivityLogElasticConfig : IElasticConfig
+    {
+        string IElasticConfig.IndexName => DataAccess.Properties.Resources.ResourceManager.GetString("UserActivityLogIndexName");
+
+        string IElasticConfig.ElastisearchUrl => DataAccess.Properties.Resources.ResourceManager.GetString("ElasticsearchUrl");
+    }
+}
