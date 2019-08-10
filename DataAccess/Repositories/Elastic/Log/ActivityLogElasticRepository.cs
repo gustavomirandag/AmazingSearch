d9e5c3c0125e5a16b023a90962e7ca5307844545
gustavomirandag <gustavomirandag@gmail.com>
2019-08-10 15:08:26 -0300

    Added more ignored files.

---
+++ DataAccess/Repositories/Elastic/Log/ActivityLogElasticRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Services.Elastic;
+using DomainModel.Interfaces.Repository.Log;
+using DomainModel.Log;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repositories.Elastic.Log
+{
+    public class ActivityLogElasticRepository<T> : ElasticRepositoryBase<T>, IActivityLogRepository<T> where T : ActivityLogBase
+    {
+        private ElasticConnection<T> _elasticConnection;
+
+        public ActivityLogElasticRepository(ElasticConnection<T> elasticConnection)
+            :base(elasticConnection)
+        {
+        }
+    }
+}
---
+++ DomainModel/Interfaces/Repository/Log/IActivityLogRepository.cs
@@ -0,0 +1,11 @@
+﻿using DomainModel.Log;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.Interfaces.Repository.Log
+{
+    public interface IActivityLogRepository<T> : IRepository<T> where T : ActivityLogBase
+    {
+    }
+}
